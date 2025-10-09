dc6ce399d30a4ac2ce38d3729c62f44285f55757
Adeel-Ul-Rehman <ajadeel229@gmail.com>
2025-10-09 13:36:56 +0500

    http to https

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import HeroImages from "./pages/HeroImages";
 import { AppProvider, AppContext } from "./context/AppContext";
 
 // Define backendURL for API calls
-export const backendURL = "http://139.59.64.199";
+export const backendURL = "https://139.59.64.199";
 
 const AppContent = () => {
   const { admin } = useContext(AppContext);
