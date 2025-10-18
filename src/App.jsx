aea69ced3b1994b1ef4be5f5def353250f9e2ba9
Adeel Aj <146657889+Adeel-Ul-Rehman@users.noreply.github.com>
2025-10-19 04:07:25 +0500

    digital ocean ip

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import HeroImages from "./pages/HeroImages";
 import { AppProvider, AppContext } from "./context/AppContext";
 
 // Define backendURL for API calls
-export const backendURL = "https://api.hadibookstore.shop";
+export const backendURL = "http://139.59.64.199:4000";
 
 const AppContent = () => {
   const { admin } = useContext(AppContext);
