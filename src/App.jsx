79b70e4c4db0b9a3abfc2a7734794a9f9297fc8b
Adeel Aj <146657889+Adeel-Ul-Rehman@users.noreply.github.com>
2025-10-19 04:04:37 +0500

    back to DO

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import HeroImages from "./pages/HeroImages";
 import { AppProvider, AppContext } from "./context/AppContext";
 
 // Define backendURL for API calls
-export const backendURL = "https://hadi-books-store-backend-5.onrender.com";
+export const backendURL = "https://api.hadibookstore.shop";
 
 const AppContent = () => {
   const { admin } = useContext(AppContext);
